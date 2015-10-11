09ad01d8d5517c497e0caff97103e00c8b244afe
Lauren Gamboa <[setton77@gmail.com]>
2015-10-10 19:50:12 -0700

    method added: methodCheck

--- src/main/java/edu/csupomona/cs480/controller/WebController.java
+++ src/main/java/edu/csupomona/cs480/controller/WebController.java
@@ -52,6 +52,13 @@ public class WebController {
 		return "OK";
 	}
 
+	@RequestMapping(value = "/cs480/voltaire", method = RequestMethod.GET)
+	String methodCheck() {
+		// You can replace this with other string,
+		// and run the application locally to check your changes
+		// with the URL: http://localhost:8080/cs480/voltaire
+		return "Praise the Sun!";
+	}
 	/**
 	 * This is a simple example of how to use a data manager
 	 * to retrieve the data and return it as an HTTP response.
