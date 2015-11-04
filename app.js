91742f246b6e37e51bafc0a8b80f50bf1b6541b8
dmongiello <dmongiello@yahoo.com>
2015-11-03 20:09:50 -0800

    Changed port to 8080

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ passport.deserializeUser(function(username, done) {
                                                                            done(null, user);
                                                                            });
                          });
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(cookieParser());
