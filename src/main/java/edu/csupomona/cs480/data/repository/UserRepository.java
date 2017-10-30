b16c5d0b0d9ee821b2f84f2aaf76e49ac6e20024
Victor Darkes <josephdarkes@gmail.com>
2017-10-29 22:46:32 -0700

    added comments to repository classes

--- src/main/java/edu/csupomona/cs480/data/repository/UserRepository.java
+++ src/main/java/edu/csupomona/cs480/data/repository/UserRepository.java
@@ -3,5 +3,11 @@ package edu.csupomona.cs480.data.repository;
 import edu.csupomona.cs480.data.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * CrudRepository is an interface that gives access to CRUD operations without
+ * the need to write them ourselves.
+ *
+ * Each method that begins with "findBy" is a query in the repository for the specified element.
+ */
 public interface UserRepository extends CrudRepository<User, Integer>{
 }
