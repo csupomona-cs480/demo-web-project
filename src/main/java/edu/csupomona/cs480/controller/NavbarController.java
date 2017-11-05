1e03e697499de9e68c8b64dba811c055bf629c4d
Bao <chauquocbao0907@gmail.com>
2017-11-04 22:22:16 -0700

    deploy front end with back end. delete unused template files

--- src/main/java/edu/csupomona/cs480/controller/NavbarController.java
+++ src/main/java/edu/csupomona/cs480/controller/NavbarController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class NavbarController {
 
     @RequestMapping("/")
     String index() {
         return "index";
+    }
+    
+    @RequestMapping("/create")
+    String create() {
+    	return "index";
     }
 }
