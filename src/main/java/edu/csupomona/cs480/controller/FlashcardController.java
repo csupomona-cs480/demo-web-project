9b9da74a7054fb3f0242f9e50d36387a52e2dd55
Victor Darkes <josephdarkes@gmail.com>
2017-11-10 21:05:15 -0800

    added GET and POST for flashcard sets

--- src/main/java/edu/csupomona/cs480/controller/FlashcardController.java
+++ src/main/java/edu/csupomona/cs480/controller/FlashcardController.java
@@ -1,15 +1,18 @@
 package edu.csupomona.cs480.controller;
 
+import com.sun.org.apache.regexp.internal.RE;
 import edu.csupomona.cs480.data.entity.Flashcard;
 import edu.csupomona.cs480.service.FlashcardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@RestController
 public class FlashcardController {
 
     private FlashcardService flashcardService;
@@ -41,10 +44,17 @@ public class FlashcardController {
     }
 
     //List all
-    @RequestMapping(value = "flashcards")
-    public String list(Model model) {
-        model.addAttribute("flashcards", flashcardService.listAllFlashcards());
-        return "flashcards";
+    @RequestMapping(value = "flashcards", method = RequestMethod.GET)
+    public List<Flashcard> list(Model model) {
+        //model.addAttribute("flashcards", flashcardService.listAllFlashcards());
+
+        List<Flashcard> list = new ArrayList<>();
+        Iterator f = flashcardService.listAllFlashcards().iterator();
+        while(f.hasNext()) {
+            list.add((Flashcard) f.next());
+        }
+
+        return list;
     }
 
     //Update
@@ -68,9 +78,21 @@ public class FlashcardController {
         return "flashcards";
     }
 
-//    @RequestMapping("user/flashcards/{setId}")
-//    public String listFlashcardSet(@PathVariable String setId, Model model) {
-//        model.addAttribute("flashcards", flashcardService.listAllFlashcardsBySetId(setId));
-//        return "flashcardset";
-//    }
+    //return a given sets
+    @RequestMapping(value = "flashcard/set/{setId}", method = RequestMethod.GET)
+    public List<Flashcard> listFlashcardSet(@PathVariable String setId) {
+
+        return flashcardService.listAllFlashcardsBySetId(setId);
+    }
+
+    //receive a set and add to database
+    @RequestMapping(value = "flashcard/set/new", method = RequestMethod.POST, consumes = "application/json")
+    @ResponseBody
+    public void saveFlashcardSet(@RequestBody List<Flashcard> flashcard) {
+
+        for(Flashcard f : flashcard) {
+            System.out.println(f.getSetId());
+            flashcardService.saveFlashcard(f);
+        }
+    }
 }
